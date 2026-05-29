507e08b8f3959b0ebc9435c652d43ef4a7cccd7c
Royal Ertiga Admin <passenger@royalertiga.com>
2026-05-30 00:55:23 +0530

    Configure GitHub Actions deployment workflow for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Magic-Cab/',
   plugins: [
     react(),
     tailwindcss(),
   ],
 })
+
 
